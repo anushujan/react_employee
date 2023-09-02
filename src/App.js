f0936f5072d985b8db5a411e05129b3f55f50545
anushujan satkunarasa <43997407+anushujan@users.noreply.github.com>
2023-09-02 11:24:40 +0530

    base connect databse

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from 'react'
 
 function App() {
+  const [name,setName] =useState()
+  const [age,setAge] =useState(0)
+  const [country,setCountry] =useState()
+  const [position,setPosition] =useState()
+  const [salary,setSalary] =useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <div className="information">
+      <form onSubmit={console.log(name)}>
+        <label>Name</label>
+        <input type="text" name="name" onChange={(e)=>setName(e.target.value)} />
+        <label>Age</label>
+        <input type="text" name="age" onChange={(e)=>setAge(e.target.value)} />
+        <label>Country</label>
+        <input type="text" name="country" onChange={(e)=>setCountry(e.target.value)} />
+        <label>Position</label>
+        <input type="text" name="position" onChange={(e)=>setPosition(e.target.value)} />
+        <label>Salary</label>
+        <input type="text" name="salary" onChange={(e)=>setSalary(e.target.value)} />
+        <button type="submit">Add Employee</button>
+      </form>
+      </div>
+    </>
+   
   );
 }
 
